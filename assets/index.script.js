b2c16c16071d999d137082001c1bca95d5aa6597
Alineevang <aline.evangelista@aluno.senai.br>
2023-08-15 14:13:31 -0300

    feat: edit js

---
+++ assets/index.script.js
@@ -0,0 +1,6 @@
+function addList() {
+    const creatTask = document.getElementById(creatTask).value;
+    const checkTask = document.getElementById(checkTask).value;
+    const editTask = document.getElementById(editTask).value;
+    const trashTask = document.getElementById(trashTask).value;
+}
