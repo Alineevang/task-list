14901802a50f16bccf644c53ccd586bce3506e3c
Alineevang <aline.evangelista@aluno.senai.br>
2023-08-15 16:26:32 -0300

    feat: start make Javascript

--- assets/index.script.js
+++ assets/index.script.js
@@ -1,6 +1,29 @@
-function addList() {
-    const creatTask = document.getElementById(creatTask).value;
-    const checkTask = document.getElementById(checkTask).value;
-    const editTask = document.getElementById(editTask).value;
-    const trashTask = document.getElementById(trashTask).value;
+const task = [];
+let taskIndex = -1;
+
+// class structureTask {
+//     constructor(title,  )
+// }
+
+function addTask() {
+    const edit = document.getElementById("edit").value;
+    const check = document.getElementById("check").value;
+    const trash = document.getElementById("trash").value;
 }
+
+if (edit && check && trash) {
+    if (taskIndex == -1) {
+        baseTask(edit, check, trash);
+        cleanTask();
+        showTask();
+
+    } else {
+        task[taskIndex] = {
+        edit,
+        check,
+        trash,
+        };
+    }
+} 
+baseTask
+  
